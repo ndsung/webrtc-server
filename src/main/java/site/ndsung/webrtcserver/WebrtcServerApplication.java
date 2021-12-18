e92976dacea42e1d66169668d30e38947eb2cdb3
ndsung <nanchanggang1874@gmail.com>
2021-12-18 14:35:41 +0800

    init: 初始化spring-boot项目

---
+++ src/main/java/site/ndsung/webrtcserver/WebrtcServerApplication.java
@@ -0,0 +1,13 @@
+package site.ndsung.webrtcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebrtcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebrtcServerApplication.class, args);
+    }
+
+}
